00fd28246a0247f53ec940f3e5c3eb8272de16d1
odiaseilenre-soft <odiaseilenre@gmail.com>
2026-06-06 23:47:55 +0100

    Delete products when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -23,7 +23,7 @@ cart.forEach((cartItem) => {
     });
 
     cartSummaryHTML += `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
             Delivery date: Tuesday, June 21
         </div>
@@ -109,6 +109,8 @@ document.querySelectorAll('.js-delete-quantity-link')
         link.addEventListener('click', () => {
             const productId = link.dataset.productId;
             removeFromCart(productId);
+            const container = document.querySelector(`.js-cart-item-container-${productId}`);
+            container.remove();
         });
-
+        
     });
