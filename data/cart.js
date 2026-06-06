3eb648c21ef1436faa2b2e09108d9072a1efd165
odiaseilenre-soft <odiaseilenre@gmail.com>
2026-06-07 00:13:43 +0100

    Save cart to lacalStorage

--- data/cart.js
+++ data/cart.js
@@ -1,13 +1,21 @@
-export let cart = [{
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    cart = [{
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
-}, {
+    }, {
     productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
     quantity: 1
-}, {
+    }, {
     productId: '83d4ca15-0f35-48f5-b7a3-1ea210004f2e',
     quantity: 5
-}];
+    }];
+}
+
+function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -27,6 +35,7 @@ export function addToCart(productId) {
             quantity: 1
             }
         );
+        saveToStorage();
     }
 }
 
@@ -39,4 +48,6 @@ export function removeFromCart(productId) {
         }
     });
     cart = newCart;
+
+    saveToStorage();
 }
